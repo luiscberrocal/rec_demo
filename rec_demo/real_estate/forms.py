070d6e8c7dc1b1043ff9668f440e139c9ab9a8d6
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-01-04 17:55:24 -0500

    Admin putting created by and modified by as readonly

--- rec_demo/real_estate/forms.py
+++ rec_demo/real_estate/forms.py
@@ -1,1 +1,26 @@
 from django import forms
+
+from .models import Company
+
+
+class CompanyForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user')
+        super(CompanyForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = Company
+        fields = (
+            'name',
+            'short_name',
+            'logo'
+        )
+
+    def clean(self):
+        clean_data = super(CompanyForm, self).clean()
+        if self.instance is None:
+            clean_data['created_by'] = self.user
+        clean_data['updated_by'] = self.user
+        return clean_data
+
+
