6a9cc96a35a7e8bc58690567975da04dd2e883a7
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-01-05 12:38:38 -0500

    List page and adde Auditable view mixin

--- rec_demo/core/mixins.py
+++ rec_demo/core/mixins.py
@@ -1,9 +1,8 @@
 class AuditableViewMixin(object, ):
-    def form_valid(self, form, ):
-        if not form.instance.created_by:
-            form.instance.created_by = self.request.user
-        form.instance.modified_by = self.request.user
-        return super(AuditableViewMixin, self).form_valid(form)
+    def get_form_kwargs(self):
+        kwargs = super(AuditableViewMixin, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
 
 
 class AdminAuditableMixin(object):
