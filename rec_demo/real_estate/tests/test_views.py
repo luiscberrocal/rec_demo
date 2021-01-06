d90d8cf11a5f31633826a616fa9042b5621e930c
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-01-06 15:43:02 -0500

    Create contract working

--- rec_demo/real_estate/tests/test_views.py
+++ rec_demo/real_estate/tests/test_views.py
@@ -1,8 +1,9 @@
-#//contract=lowercaseAndDash(MODEL)
-#//contract=snakeCase(MODEL)
+# //contract=lowercaseAndDash(MODEL)
+# //contract=snakeCase(MODEL)
 from test_plus import TestCase
 
-from .factories import ContractFactory
+from .factories import ContractFactory, ClientFactory
+from ..forms import ContractForm
 from ..models import Contract
 from ...core.utils import model_to_json_dict
 from ...users.tests.factories import SimpleUserFactory
@@ -18,12 +19,24 @@ class TestContractCreateView(TestCase):
         contract_dict = model_to_json_dict(contract)
         contract_dict.pop('id')
         contract.delete()
+        clients = ClientFactory.create_batch(3)
+        i = 0
+        for client in clients:
+            client_field = ContractForm.CLIENT_PATTERN.format(i)
+            is_principal_field = ContractForm.IS_PRINCIPAL_PATTERN.format(i)
+            contract_dict[client_field] = client.id
+            if i == 0:
+                contract_dict[is_principal_field] = 'on'
+            i += 1
 
         with self.login(self.app_user):
             response = self.post('real_estate:create-contract', data=contract_dict)
             self.response_302(response)
 
         self.assertEqual(Contract.objects.count(), 1)
+        contract = Contract.objects.first()
+        self.assertEqual(contract.contract_clients.filter(is_principal=True).count(), 1)
+        self.assertEqual(contract.contract_clients.count(), 3)
 
     def test_get(self):
         contract = ContractFactory.create(created_by=self.app_user)
