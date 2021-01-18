ede71156ef9401bb81cf0a9a665ea06d54703fde
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-01-18 18:18:12 -0500

    Added create transaction types

--- rec_demo/real_estate/management/commands/create_types.py
+++ rec_demo/real_estate/management/commands/create_types.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand
 from ...models import RealEstateProject, Company
 from ...tests.factories import RealEstateProjectFactory, CompanyFactory
 from ...utils import get_or_create_sales_types
+from ....banking.utils import get_or_create_transaction_types
 from ....users.models import User
 
 
@@ -18,3 +19,9 @@ class Command(BaseCommand):
         self.stdout.write('--- Sales Types ---')
         for result in results:
             self.stdout.write(f'Created {result["created"]} {result["name"]}')
+
+        self.stdout.write('--- Transaction Types ---')
+        transaction_types = get_or_create_transaction_types()
+        for transaction_type in transaction_types:
+            self.stdout.write(f'Created: {transaction_type["created"]} {transaction_type["name"]}')
+
