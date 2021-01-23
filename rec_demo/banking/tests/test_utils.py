10b1f583dcb7e8a6fd9812769282cd0a6ba1671d
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-01-23 09:09:33 -0500

    Added and tested test_divide_in_payments_with_dates

--- rec_demo/banking/tests/test_utils.py
+++ rec_demo/banking/tests/test_utils.py
@@ -1,8 +1,9 @@
+from datetime import date
 from decimal import Decimal
 
 from django.test import SimpleTestCase
 
-from ..utils import divide_in_payments
+from ..utils import divide_in_payments, divide_in_payments_with_dates
 
 
 class Test_divide_in_payments(SimpleTestCase):
@@ -34,3 +35,20 @@ class Test_divide_in_payments(SimpleTestCase):
         self.assertEqual(amount, calculated_total)
         self.assertEqual(payments[0], Decimal('137.00'))
         self.assertEqual(payments[number - 1], Decimal('137.25'))
+
+    def test_divide_in_payments_with_dates(self):
+        amount = Decimal('12433.27')
+        number = 6
+        start_date = date(2020, 11, 3)
+        payments = divide_in_payments_with_dates(amount, number, start_date)
+        self.assertEqual(len(payments), number)
+
+        calculated_total = Decimal('0.00')
+        for payment in payments:
+            calculated_total += payment['amount']
+
+        self.assertEqual(amount, calculated_total)
+        self.assertEqual(payments[0]['amount'], Decimal('2072.21'))
+        self.assertEqual(payments[0]['date'], start_date)
+        self.assertEqual(payments[number - 1]['amount'], Decimal('2072.22'))
+        self.assertEqual(payments[number - 1]['date'], date(2021, 4, 3))
--- rec_demo/banking/utils.py
+++ rec_demo/banking/utils.py
@@ -1,3 +1,4 @@
+from datetime import date
 from decimal import Decimal
 
 from .exceptions import BankingException
@@ -48,6 +49,8 @@ def get_or_create_transaction_types():
 
 
 def divide_in_payments(amount, number, diff_to_last=True, **kwargs):
+    if number <= 1:
+        return amount
     rounding = kwargs.get('rounding', '1.00')
     payment = (amount / Decimal(str(number))).quantize(Decimal(rounding))
     remainder = amount % payment
@@ -60,3 +63,25 @@ def divide_in_payments(amount, number, diff_to_last=True, **kwargs):
         else:
             payments[0] = payments[0] + remainder
     return payments
+
+
+def get_next_month_date(current_date, delta=1):
+    year = current_date.year
+    month = current_date.month
+    day = current_date.day
+    next_month = month + delta
+    if 12 < next_month <= 24:
+        year += 1
+        next_month -= 12
+    return date(year, next_month, day)
+
+
+def divide_in_payments_with_dates(amount, number, start_date, diff_to_last=True, **kwargs):
+    payments_only = divide_in_payments(amount, number, diff_to_last, **kwargs)
+    payments = list()
+    current_date = start_date
+    for i in range(number):
+        payments.append({'date': current_date, 'amount': payments_only[i]})
+        current_date = get_next_month_date(current_date)
+    return payments
+
