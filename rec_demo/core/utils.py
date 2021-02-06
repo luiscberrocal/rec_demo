a4a0c17c85a31abb8ba02fe1416fb9ac401c0be7
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-02-06 15:44:09 -0500

    Eliminated the use of factories for fakes

--- rec_demo/core/utils.py
+++ rec_demo/core/utils.py
@@ -73,3 +73,32 @@ def convert_to_decimal(data):
         return data
     else:
         raise ValueError('Unsupported type to convert to decimal')
+
+
+class CaseChanger(object):
+    def filters(self):
+        return {'to_snake_case': self.to_snake_case,
+                'to_dash_case': self.to_dash_case,
+                'to_camel_case': self.to_camel_case}
+
+    def to_snake_case(self, variable_name):
+        """
+        Converts from TransactionType to transaction_type
+        :param variable_name:
+        :return:
+        """
+        name = re.sub(r'(?<!^)(?=[A-Z])', '_', variable_name).lower()
+        return name
+
+    def to_dash_case(self, variable_name):
+        """
+        Converts from TransactionType to transaction-type
+        :param variable_name:
+        :return:
+        """
+        name = re.sub(r'(?<!^)(?=[A-Z])', '-', variable_name).lower()
+        return name
+
+    def to_camel_case(self, snake_str):
+        components = snake_str.split('_')
+        return ''.join(x.title() for x in components)
