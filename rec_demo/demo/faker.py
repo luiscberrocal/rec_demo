ca205e3bab96a8b8b0edd4631a30e4b1e57111c9
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-02-14 08:42:09 -0500

    Delete brokers and clients

--- rec_demo/demo/faker.py
+++ rec_demo/demo/faker.py
@@ -2,9 +2,10 @@ import json
 import os
 
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
-from rec_demo.real_estate.exceptions import RealEstateException
-from rec_demo.real_estate.models import Client, Broker
+from ..real_estate.exceptions import RealEstateException
+from ..real_estate.models import Client, Broker
 
 
 def create_clients(**kwargs):
@@ -20,15 +21,25 @@ def create_broker(**kwargs):
 def _create_people(RECModel, **kwargs):
     model_name = RECModel.__name__.lower()
     filename = kwargs.get('filename', settings.APPS_DIR / f'demo/fake_{model_name}s.json')
+    delete = kwargs.get('delete', False)
+    user = kwargs.get('user', get_user_model().objects.first())
+    if user is None:
+        msg = 'A user must exist in the database for the created_by attribute'
+        raise RealEstateException(msg)
     if not os.path.exists(filename):
         raise RealEstateException(f"File {filename} not found for {model_name}")
     with open(filename, 'r', encoding='utf-8') as json_file:
         model_list = json.load(json_file)
     for model_data in model_list:
+        model_data['created_by'] = user
         try:
             rec_model = RECModel.objects.get(national_id=model_data['national_id'])
-            model_data['created'] = False
-            model_data['id'] = rec_model.id
+            if delete:
+                rec_model.delete()
+                raise RECModel.DoesNotExist
+            else:
+                model_data['created'] = False
+                model_data['id'] = rec_model.id
         except RECModel.DoesNotExist:
             model_data['full_name'] = f'{model_data["first_name"]} {model_data["last_name"]}'
             rec_model = RECModel.objects.create(**model_data)
--- rec_demo/demo/tests/test_faker.py
+++ rec_demo/demo/tests/test_faker.py
@@ -2,9 +2,14 @@ from test_plus import TestCase
 
 from ..faker import create_clients, create_broker
 from ...real_estate.models import Client, Broker
+from ...users.tests.factories import SimpleUserFactory
 
 
 class TestFaker(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user1 = SimpleUserFactory.create()
 
     def test_create_clients(self):
         clients = create_clients()
@@ -14,6 +19,7 @@ class TestFaker(TestCase):
             self.assertEqual(client['created'], True)
         self.assertEqual(Client.objects.filter(client_type=Client.JURIDICAL_TYPE).count(), 1)
         self.assertEqual(Client.objects.filter(client_type=Client.NATURAL_TYPE).count(), 5)
+        self.assertEqual(Client.objects.filter(created_by__isnull=True).count(), 0)
 
     def test_create_broker(self):
         brokers = create_broker()
@@ -23,4 +29,18 @@ class TestFaker(TestCase):
             self.assertEqual(broker['created'], True)
         self.assertEqual(Broker.objects.filter(broker_type=Broker.JURIDICAL_TYPE).count(), 1)
         self.assertEqual(Broker.objects.filter(broker_type=Broker.NATURAL_TYPE).count(), 5)
+        self.assertEqual(Broker.objects.filter(created_by__isnull=True).count(), 0)
+
+    def test_create_broker_with_delete(self):
+        create_broker()
+        self.assertEqual(Broker.objects.count(), 6)
+        brokers = create_broker(delete=True)
+        self.assertEqual(len(brokers), 6)
+        for broker in brokers:
+            self.assertIsNotNone(broker['id'])
+            self.assertEqual(broker['created'], True)
+        self.assertEqual(Broker.objects.filter(broker_type=Broker.JURIDICAL_TYPE).count(), 1)
+        self.assertEqual(Broker.objects.filter(broker_type=Broker.NATURAL_TYPE).count(), 5)
+        self.assertEqual(Broker.objects.filter(created_by__isnull=True).count(), 0)
+
 
